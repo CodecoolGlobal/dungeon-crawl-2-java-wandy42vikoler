ac878b8f569e59203730b35733f11e9e545face0
mscobblepot <tomecsko.reka@gmail.com>
2022-06-14 10:00:35 +0200

    Adds items to map

--- src/main/java/com/codecool/dungeoncrawl/logic/MapLoader.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/MapLoader.java
@@ -2,6 +2,8 @@ package com.codecool.dungeoncrawl.logic;
 
 import com.codecool.dungeoncrawl.logic.actors.Player;
 import com.codecool.dungeoncrawl.logic.actors.Skeleton;
+import com.codecool.dungeoncrawl.logic.items.Key;
+import com.codecool.dungeoncrawl.logic.items.Sword;
 
 import java.io.InputStream;
 import java.util.Scanner;
@@ -39,6 +41,15 @@ public class MapLoader {
                             cell.setType(CellType.FLOOR);
                             map.setPlayer(new Player(cell));
                             break;
+                        case '+':
+                            cell.setType(CellType.FLOOR);
+                            new Sword(cell);
+                            break;
+                        case '§':
+                            cell.setType(CellType.FLOOR);
+                            new Key(cell);
+                            break;
+
                         default:
                             throw new RuntimeException("Unrecognized character: '" + line.charAt(x) + "'");
                     }
